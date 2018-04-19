c210650ef77adda12618123b3245cea77e9ecf8e
Trevor Frizzell <trevor@frizzell.ca>
2018-04-19 00:15:56 -0300

    Started work on cron jobs for fetching updates and pushing messages out

--- duthie-bot.js
+++ duthie-bot.js
@@ -1,25 +1,67 @@
 'use strict';
 
-const child_process = require('child_process');
-const cron = require('cron');
-const Discord = require('discord.js');
 
+///////////////////////////////////////
+//     GLOBAL VARIABLE CREATION      //
+///////////////////////////////////////
 global.config = global.config || require('./config.json');
 global.db = global.db || require('./lib/db');
 global.logger = global.logger || require('./lib/logger');
 global.pkg = global.pkg || require('./package.json');
 
+
+///////////////////////////////////////
+//    PREPARED STATEMENT REGISTRY    //
+///////////////////////////////////////
+const stmts = [];
+
+global.prepareStatement = (...args) => {
+	const stmt = db.prepare(...args);
+	const finalize = stmt.finalize.bind(stmt);
+
+	stmt.finalize = (...args) => {
+		const result = finalize(...args);
+		stmts.splice(stmts.indexOf(stmt), 1);
+		return result;
+	};
+
+	stmts.push(stmt);
+	return stmt;
+};
+
+
+///////////////////////////////////////
+//         LOAD DEPENDENCIES         //
+///////////////////////////////////////
+const Discord = require('discord.js');
+
+
+///////////////////////////////////////
+//          STARTUP MESSAGE          //
+///////////////////////////////////////
 logger.info(`Starting ${config.name} v${pkg.version.replace(/^v+/g, '')} with node.js v${process.version.replace(/^v+/g, '')}, discord.js v${Discord.version.replace(/^v+/g, '')}`);
 
-const client = new Discord.Client();
-require('./lib/discord')(client);
+
+///////////////////////////////////////
+//   DISCORD CLIENT INITIALIZATION   //
+///////////////////////////////////////
+global.client = new Discord.Client();
+const cleanupClient = require('./lib/discord')(client);
 
 client.login(config.token).then(() => {
 	logger.info('Opened connection to Discord');
 });
 
-/** Install cron jobs **/
 
+///////////////////////////////////////
+//             CRON JOBS             //
+///////////////////////////////////////
+require('./lib/cron')({client, db});
+
+
+///////////////////////////////////////
+//     EVENT AND SIGNAL HANDLERS     //
+///////////////////////////////////////
 process.on('exit', () => {
 	logger.info(`Shutting down ${config.name} v${pkg.version.replace(/^v+/g, '')}`);
 });
@@ -32,13 +74,17 @@ require('./lib/node/cleanup')(() => Promise.all([
 			return resolve();
 		}
 
-		client.destroy().then(() => {
+		client.destroy().then(err => {
+			if (err) {
+				logger.error(err);
+			}
+
 			logger.info('Closed connection to Discord');
 			resolve();
 		}).catch(err => {
 			logger.error(err);
 			resolve();
-		})
+		});
 	}),
 	new Promise(resolve => {
 		if (!db.open) {
@@ -51,14 +97,17 @@ require('./lib/node/cleanup')(() => Promise.all([
 			}
 		});
 
+		while (stmts.length > 0) {
+			stmts[0].finalize();
+		}
+
 		db.close(err => {
 			if (err) {
 				logger.error(err);
-			} else {
-				logger.info('Closed connection to database');
 			}
 
+			logger.info('Closed connection to database');
 			resolve();
-		})
+		});
 	})
 ]));
