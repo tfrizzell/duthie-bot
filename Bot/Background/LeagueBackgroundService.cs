bd92fbd02da0cf88ac3c552d8486aa7b3ec3ee54
Trevor Frizzell <trevor.frizzell@mlt.ca>
2022-06-07 08:59:12 -0300

    Added game removal when leagues change seasons

--- Bot/Background/LeagueBackgroundService.cs
+++ Bot/Background/LeagueBackgroundService.cs
@@ -1,7 +1,10 @@
 using System.Diagnostics;
+using System.Reflection;
 using Duthie.Services.Api;
+using Duthie.Services.Games;
 using Duthie.Services.Leagues;
 using Duthie.Types.Api;
+using Duthie.Types.Leagues;
 using Microsoft.Extensions.Logging;
 
 namespace Duthie.Bot.Background;
@@ -11,15 +14,18 @@ public class LeagueBackgroundService : ScheduledBackgroundService
     private readonly ILogger<LeagueBackgroundService> _logger;
     private readonly ApiService _apiService;
     private readonly LeagueService _leagueService;
+    private readonly GameService _gameService;
 
     public LeagueBackgroundService(
         ILogger<LeagueBackgroundService> logger,
         ApiService apiService,
-        LeagueService leagueService) : base(logger)
+        LeagueService leagueService,
+        GameService gameService) : base(logger)
     {
         _logger = logger;
         _apiService = apiService;
         _leagueService = leagueService;
+        _gameService = gameService;
     }
 
     protected override string[] Schedules
@@ -39,24 +45,57 @@ public class LeagueBackgroundService : ScheduledBackgroundService
         {
             var leagues = await _leagueService.GetAllAsync();
 
-            await Task.WhenAll(leagues.Select(async league =>
+            var seasonChanges = (await Task.WhenAll(leagues.Select(async league =>
             {
                 var api = _apiService.Get<ILeagueApi>(league);
 
                 if (api == null)
-                    return;
+                    return null;
 
                 var data = await api.GetLeagueAsync(league);
 
                 if (data == null)
-                    return;
+                    return null;
+
+                var seasonIdProp = league.Info?.GetType().GetProperty("SeasonId", BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+
+                if (seasonIdProp != null)
+                {
+                    var prevSeasonId = seasonIdProp.GetValue(league.Info);
+                    var nextSeasonId = seasonIdProp.GetValue(data.Info);
+
+                    if (!Object.Equals(prevSeasonId, nextSeasonId))
+                    {
+                        return new SeasonChange
+                        {
+                            League = league,
+                            PrevSeasonId = prevSeasonId,
+                            NextSeasonId = nextSeasonId
+                        };
+                    }
+                }
 
                 league.Name = data.Name;
                 league.LogoUrl = data.LogoUrl;
                 league.Info = data.Info;
-            }));
+                return null;
+            })))
+            .Where(l => l != null)
+            .Cast<SeasonChange>();
 
             await _leagueService.SaveAsync(leagues);
+
+            foreach (var change in seasonChanges)
+            {
+                _logger.LogDebug($"League \"{change.League.Name}\" [{change.League.Id}] has changed seasons from {change.PrevSeasonId} to {change.NextSeasonId}");
+                var games = await _gameService.GetAllAsync(change.League.Id);
+
+                if (games.Count() > 0)
+                {
+                    await _gameService.DeleteAsync(games.Select(g => g.Id));
+                    _logger.LogTrace($"Deleted {games.Count()} games for league \"{change.League.Name}\" [{change.League.Id}]");
+                }
+            }
 
             sw.Stop();
             _logger.LogTrace($"League update task completed in {sw.Elapsed.TotalMilliseconds}ms");
@@ -67,5 +106,12 @@ public class LeagueBackgroundService : ScheduledBackgroundService
             _logger.LogTrace($"League update task failed in {sw.Elapsed.TotalMilliseconds}ms");
             _logger.LogError(e, "An unexpected error during league update task.");
         }
+    }
+
+    private class SeasonChange
+    {
+        internal League League { get; set; } = default!;
+        internal object? PrevSeasonId { get; set; }
+        internal object? NextSeasonId { get; set; }
     }
 }
